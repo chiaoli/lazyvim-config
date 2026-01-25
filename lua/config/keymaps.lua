8d6d52ed70227038c3019f2e7e6650d22a33e742
chiaoli <hussar91522@gmail.com>
2026-01-25 15:32:11 +0800

    Feat(keymaps): add <leader>/ for toggle comment
    新增 <leader>/ 快捷鍵來切換註解，符合原本使用習慣
    - Normal 模式：切換當前行註解
    - Visual 模式：切換選取範圍的註解

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Toggle comment with <leader>/
+vim.keymap.set("n", "<leader>/", "gcc", { remap = true, desc = "Toggle Comment" })
+vim.keymap.set("v", "<leader>/", "gc", { remap = true, desc = "Toggle Comment" })
